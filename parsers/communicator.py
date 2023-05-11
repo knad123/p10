036ff980e87049aa4126707bdb8920e6a17e3575
lasse <lkar18@student.aau.dk>
2023-05-11 09:41:19 +0200

    made another attempt at fixing json loading bug

--- parsers/communicator.py
+++ parsers/communicator.py
@@ -14,21 +14,34 @@ import os
 def update_demands_and_paths(simulation_dir: str, network: MLPS_Network, essence_state: EssenceState, recorder, conf):
     if conf["algorithm"] == "essence_stateless":
         essence_state.current_population = []
-
-    with open("demands.json", "r") as file:
-        content = file.read()
-        demands_data = json.loads(content)
-
+    demands_loaded = False
+    while not demands_loaded:
+        try:
+            with open("demands.json", "r") as file:
+                content = file.read()
+                demands_data = json.loads(content)
+                demands_loaded = True
+        except:
+            print("Failed to load demands, retrying..")
+            time.sleep(30)
     # Used to set the weight of congestion and stretch
-    with open("utilization.json", "r") as file:
-        utilizations_data = json.load(file)
-        max_utilization = 0
-        for key, value in utilizations_data.items():
-            if key != 'timestamp':
-                for inner_key, inner_value in value.items():
-                    if inner_value > max_utilization:
-                        max_utilization = inner_value
-    essence_state.congestion_weight = max_utilization
+    utilization_loaded = False
+
+    while not utilization_loaded:
+        try:
+            with open("utilization.json", "r") as file:
+                utilizations_data = json.load(file)
+                max_utilization = 0
+                for key, value in utilizations_data.items():
+                    if key != 'timestamp':
+                        for inner_key, inner_value in value.items():
+                            if inner_value > max_utilization:
+                                max_utilization = inner_value
+            essence_state.congestion_weight = max_utilization
+            utilization_loaded = True
+        except:
+            print("Failed to load utilization, retrying..")
+            time.sleep(30)
 
     demands: Dict[(str,str), float] = import_demands(demands_data)
     network.demands.update(demands)
